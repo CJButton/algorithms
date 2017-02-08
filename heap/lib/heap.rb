6bfcd59734aa52ee6ce4a6888e0a18360b801046
CJButton <gsyme1907@gmail.com>
2017-02-08 13:58:33 -0800

    push works correctly for minHeap

--- heap/lib/heap.rb
+++ heap/lib/heap.rb
@@ -11,12 +11,23 @@ class BinaryMinHeap
   end
 
   def extract
+    raise "no element to extract" if count == 0
+
+    @store[0], @store[count - 1] = @store[count - 1], @store[0]
+    extracted = @store.pop
+    BinaryMinHeap.heapify_down(@store, 0, &prc)
+
   end
 
   def peek
+
   end
 
   def push(val)
+    @store << val
+
+    self.class.heapify_up(@store,
+    @store.length - 1, @store.length, &prc)
   end
 
   protected
@@ -35,37 +46,45 @@ class BinaryMinHeap
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
-    prc ||= Proc.new {|a, b| (a <=> b) }
+    prc ||= Proc.new { |a, b| a <=> b }
 
-    children = BinaryMinHeap.child_indices(len, idx)
+    left, right = child_indices(len, parent_idx)
+    children = []
+    children.push(array[left]) if left
+    children.push(array[right]) if right
 
-    # array.each_with_index do |parent, idx1|
-    #   children = BinaryMinHeap.child_indices(len, idx1)
-    #
-    #   return array if children.empty?
-    #   children.each do |idx2|
-    #     if array[idx1] > array[idx2]
-    #       array[idx1], array[idx2] = array[idx2], array[idx1]
-    #       break
-    #     end
-    #   end
-    # end
+    # if all of the child nodes are less than the parent, then we
+    # are in the right spot, and should return the array
+    if children.all? {|node| prc.call(array[parent_idx], node) <= 0}
+      return array
+    end
 
+    # we want to swap with the smaller child node (if it exists)
+    # but leave the option for a proc if supplied
+    swap_idx = nil
+    if children.length == 1
+      swap_idx = left
+    else
+      swap_idx = prc.call(children[0], children[1]) == -1 ? left : right
+    end
+
+    array[parent_idx], array[swap_idx] = array[swap_idx], array[parent_idx]
+
+    heapify_down(array, swap_idx, len, &prc)
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
-    prc ||= Proc.new {|a, b| -1 * (a <=> b) }
+    prc ||= Proc.new { |a, b| a <=> b }
 
-    array.each_with_index do |parent, idx1|
-      children = BinaryMinHeap.child_indices(len, idx1)
+    return array if child_idx == 0
 
-      return array if children.empty?
-      children.each do |idx2|
-        if array[idx1] > array[idx2]
-          array[idx1], array[idx2] = array[idx2], array[idx1]
-          break
-        end
-      end
+    parent_idx = parent_index(child_idx)
+
+    if prc.call(array[parent_idx], array[child_idx]) == 1
+      array[parent_idx], array[child_idx] = array[child_idx], array[parent_idx]
+      heapify_up(array, parent_idx, len, &prc)
+    else
+      array
     end
 
   end
