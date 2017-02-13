9ec3476fecc433534a33e5c4b287bf8810402d10
CJButton <gsyme1907@gmail.com>
2017-02-12 23:15:03 -0800

    quicksort needs to be more efficient

--- quicksort/quicksort_skeleton/lib/quick_sort.rb
+++ quicksort/quicksort_skeleton/lib/quick_sort.rb
@@ -32,13 +32,44 @@ class QuickSort
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
-    p "initial array is #{array}"
-    new_start = QuickSort.partition(array, start, length, &prc)
-    p array
-    p new_start
+    return array if length < 2
+    # better solution is...
+    pivot = partition(array, start, length, &prc)
+    left = pivot - start
+    right =  pivot + 1
+
+
+    # return array if length < 2
+    #
+    # pivot_idx = partition(array, start, length, &prc)
+    #
+    # left_length = pivot_idx - start
+    # right_length = length - (left_length + 1)
+    # sort2!(array, start, left_length, &prc)
+    # sort2!(array, pivot_idx + 1, right_length, &prc)
+
+
+    # I sense this is really not an ideal solution
+    # too many comparisons are getting made, although
+    # it does work
+
+    # swap = true
+    # until swap == false
+    #   swap = false
+    #
+    #   (start).upto(array.length - 2) do |i|
+    #     if array[i] > array[i + 1]
+    #       swap = true
+    #       QuickSort.partition(array, i, length, &prc)
+    #     end
+    #   end
+    # end
+    # array
+
   end
 
   def self.partition(array, start, length, &prc)
+    p "initial #{array}"
     prc ||= Proc.new {|a, b| a <=> b }
 
     pivot = start
@@ -58,6 +89,8 @@ class QuickSort
         wall = idx if array[idx] > array[pivot] && wall.nil?
       end
     end
+    p "swapped #{array}"
+    puts
     pivot
   end
 
