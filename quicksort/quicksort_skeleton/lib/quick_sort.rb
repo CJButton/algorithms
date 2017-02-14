fda9b769f6e33b18c5fb6d4bcfc9b7977b29dd9b
CJButton <gsyme1907@gmail.com>
2017-02-13 16:47:39 -0800

    cleaned up code a bit

--- quicksort/quicksort_skeleton/lib/quick_sort.rb
+++ quicksort/quicksort_skeleton/lib/quick_sort.rb
@@ -33,7 +33,7 @@ class QuickSort
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
     return array if length < 2
-    # better solution is...
+
     pivot = partition(array, start, length, &prc)
     # we need to know the smaller, partitioned lengths
     left = pivot - start
@@ -41,36 +41,11 @@ class QuickSort
     sort2!(array, start, left, &prc)
     sort2!(array, pivot + 1, right, &prc)
     array
-    # return array if length < 2
-    #
-    # pivot_idx = partition(array, start, length, &prc)
-    #
-    # left_length = pivot_idx - start
-    # right_length = length - (left_length + 1)
-    # sort2!(array, start, left_length, &prc)
-    # sort2!(array, pivot_idx + 1, right_length, &prc)
 
   end
 
   def self.partition(array, start, length, &prc)
     prc ||= Proc.new { |el1, el2| el1 <=> el2 }
-
-   # To reduce probability of pathalogically bad data set, shuffle pivot.
-   # new_pivot = start + rand(length)
-   # array[start], array[new_pivot] = array[new_pivot], array[start]
-   #
-  #  pivot_idx = start
-  #  pivot = array[start]
-   #
-  #  ((start + 1)...(start + length)).each do |idx|
-  #    if prc.call(pivot, array[idx]) > 0
-  #      array[idx], array[pivot_idx + 1] = array[pivot_idx + 1], array[idx]
-  #      pivot_idx += 1
-  #    end
-  #  end
-  #  array[start], array[pivot_idx] = array[pivot_idx], array[start]
-  #  pivot_idx
-
 
     p "initial #{array}"
     prc ||= Proc.new { |a, b| a <=> b }
