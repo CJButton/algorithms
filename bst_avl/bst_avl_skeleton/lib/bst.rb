39bf5b6adeda355eca2f1a7e1fbd9c9af9ff1db9
CJButton <gsyme1907@gmail.com>
2017-02-25 07:18:35 +0900

    delete min and inorder work

--- bst_avl/bst_avl_skeleton/lib/bst.rb
+++ bst_avl/bst_avl_skeleton/lib/bst.rb
@@ -1,3 +1,6 @@
+require 'byebug'
+
+
 class BSTNode
   attr_accessor :left, :right
   attr_reader :value
@@ -77,6 +80,14 @@ class BinarySearchTree
   end
 
   def self.inorder!(node)
+    return [] unless node
+
+    arr = []
+    arr += BinarySearchTree.inorder!(node.left) if node.left
+    arr << node.value
+    arr += BinarySearchTree.inorder!(node.right) if node.right
+
+    arr
 
   end
 
@@ -127,22 +138,28 @@ class BinarySearchTree
   end
 
   def self.delete_min!(node)
-    return nil if node.nil?
-    return nil if node.left.nil?
+    return nil unless node
+    return node.right unless node.left
 
-    parent_node = nil
-    min_node = node
-    unless node.left.nil?
-      loop do
-        parent_node = min_node
-        min_node = node.left
-        break if min_node.left.nil?
-      end
-    end
+    node.left = BinarySearchTree.delete_min!(node.left)
+    node
+    # return nil if node.nil?
 
-    unless min_node.right.nil?
-      parent_node.left = min_node.right
-    end
+
+    # parent_node = nil
+    # min_node = node
+    #
+    # unless node.left.nil?
+    #   loop do
+    #     parent_node = min_node
+    #     min_node = node.left
+    #     break if min_node.left.nil?
+    #   end
+    # end
+    #
+    # unless min_node.right.nil?
+    #   parent_node.left = min_node.right
+    # end
 
   end
 
@@ -154,7 +171,6 @@ class BinarySearchTree
 
         if value <= node.value
           node = node.left
-          # BinarySearchTree.find_parent(node.left, value)
         end
       end
 
@@ -163,7 +179,6 @@ class BinarySearchTree
 
         if value > node.value
           node = node.right
-          # BinarySearchTree.find_parent(node.right, value)
         end
       end
     end
@@ -178,13 +193,47 @@ class BinarySearchTree
     # 3 cases:
     # 1. no children
     if child.right.nil? && child.left.nil?
-      parent.left == child ? parent.left = nil : parent.right = nil
+      return parent.left == child ?
+              parent.left = nil : parent.right = nil
     end
 
-    # 2. one child
-    # 3. two children!
+    min_el = BinarySearchTree.min(child)
+    min_el_parent = BinarySearchTree.find_parent(min_el, min_el.value)
 
+    if parent.left == child
+      # 2. one child
+      if (child.right.nil? && !child.left.nil?)
+        parent.left = child.left
+      elsif (!child.right.nil? && child.left.nil?)
+        parent.left = child.right
+      # 3. two children!
+      elsif !child.right.nil? && !child.left.nil?
+        min_el.left, min_el.right = child.left, child.right
 
+        parent.left = min_el
+
+        min_el_parent.left == min_el ?
+          min_el_parent.left = nil :
+          min_el_parent.right = nil
+      end
+
+    elsif parent.right == child
+      # 2. one child
+      if (!child.right.nil? && child.left.nil?)
+        parent.right = child.right
+      elsif (child.right.nil? && !child.left.nil?)
+        parent.right = child.left
+      # 3. two children!
+      elsif !child.right.nil? && !child.left.nil?
+        min_el.left, min_el.right = child.left, child.right
+
+        parent.right = min_el
+
+        min_el_parent.left == min_el ?
+          min_el_parent.left = nil :
+          min_el_parent.right = nil
+      end
+    end
 
   end
 
