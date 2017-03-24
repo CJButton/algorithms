61ba499e201138b1c9ea154e3b95e5afddf77e4b
CJButton <gsyme1907@gmail.com>
2017-03-24 08:27:43 -0700

    digital root js finished

--- whiteboard problems/wb_practice_js.js
+++ whiteboard problems/wb_practice_js.js
@@ -1,16 +1,31 @@
 
 
 
-
-function digital_root(num) {
-
-  
+function get_digits(num) {
+  let digits = [];
 
   while (num > 0) {
+    digits.push(num % 10);
+    num = Math.floor(num / 10);
+  }
 
+  return digits;
+
+}
+
+function digital_root(num) {
+  let digits = get_digits(num);
+
+  let root = digits.reduce(function(acc, num) {
+    return acc + num;
+  })
+
+  if (root < 10) {
+    return root;
+  } else {
+    digital_root(root);
   }
 
 
 }
-
-console.log(digital_root(12345));
+digital_root(12345);
