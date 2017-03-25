6dc64c3cfa220e34e77319fd9de499bffc31d9ed
CJButton <gsyme1907@gmail.com>
2017-03-24 19:57:08 -0700

    alternative version of caesar cypher completed in ruby

--- whiteboard problems/wb_practice_rb_1a.rb
+++ whiteboard problems/wb_practice_rb_1a.rb
@@ -24,28 +24,58 @@ def digital_root(num)
 
 end
 
+# successful
 # digital_root(12345)
 
 #-----------
 # Caesar Shift
 
-def caesar_shift(phrase)
+
+
+def encode_word(word, shift)
+  # we create an array of all 26 letters in the alphabet
+  alphabet = ("a".."z").to_a
+
+  # the entire alphabet is now shifted over properly, with the earlier
+  # letters, now appearing at the back
+  shifted_alphabet = (alphabet[shift]..alphabet[-1]).to_a +
+                                  (alphabet[0]...alphabet[shift]).to_a
+  encoded = [] # and an empty array to place each letter into later
+
+  word.each_char do |letter|
+    if alphabet.include?(letter.downcase)
+      # find where the orignal letter's index should be
+      letter_idx = alphabet.find_index(letter.downcase)
+
+      # if the original letter was lower case, the shifted letter straight
+      # in; otherwise, we capitalize the shifted letter and place it in
+
+      letter == letter.downcase ? encoded.push(shifted_alphabet[letter_idx]) :
+                  encoded.push(shifted_alphabet[letter_idx].upcase)
+      else
+        encoded.push(letter)
+    end
+  end
+
+  encoded.join("")
+end
+
+def caesar_shift(phrase, shift)
+
+  encoded = []
+
+  words = phrase.split(" ")
+  words.each_with_index do |word, idx|
+    encoded.push(encode_word(word, shift))
+  end
+
+  encoded.join(" ")
+
 
 
 end
-
-
-def encode_word(word)
-
-
-
-
-
-
-
-
-
-
-
-
-end
+# all successful
+# p caesar_shift("abcd", 1) # => "bcde"
+# p caesar_shift("abc def", 1) # => "bcd efg"
+# p caesar_shift("z", 1) # => "a"
+# p caesar_shift("hyphenated-woRd! and SpaCEs", 1)
