f769f4f6df44e020d5af2814b6cca760aa24f573
CJButton <gsyme1907@gmail.com>
2017-04-04 22:36:19 -0700

    folding cypher js finished

--- whiteboard problems/wb_practice_3.js
+++ whiteboard problems/wb_practice_3.js
@@ -9,13 +9,39 @@ Array.prototype.myMap = function(func) {
   for (let i = 0; i < this.length; i++) {
     map.push(func(this[i]));
   }
-  console.log(map);
   return map;
-
 }
 
 
-var numbers = [1, 5, 10, 15];
-var roots = numbers.myMap(function(x) {
-   return x * 2;
-});
+// var numbers = [1, 5, 10, 15];
+// var roots = numbers.myMap(function(x) {
+//    return x * 2;
+// });
+function foldingCipher(str) {
+
+  let top = [];
+  let bottom = [];
+
+  for (let i = "a".charCodeAt(); i < "z".charCodeAt() + 1; i++) {
+    if ( i < ("z".charCodeAt() - 12)) {
+      top.push(String.fromCharCode(i));
+    } else {
+      bottom.unshift(String.fromCharCode(i));
+    }
+  }
+
+  let ciphered = [];
+  str.split("").forEach((letter) => {
+    if (letter.charCodeAt() < "z".charCodeAt() - 13) {
+      letterIdx = top.indexOf(letter.toLowerCase());
+      ciphered.push(bottom[letterIdx]);
+    } else {
+      letterIdx = bottom.indexOf(letter.toLowerCase());
+      ciphered.push(top[letterIdx]);
+    }
+  });
+  return ciphered.join("");
+
+}
+
+console.log(foldingCipher("abcdef"));
