84e61dea2f0e03530fe2ca4da96e0bf93e4c6a31
CJButton <gsyme1907@gmail.com>
2017-04-05 22:06:16 -0700

    completed mergesort

--- whiteboard problems/wb_practice_5.js
+++ whiteboard problems/wb_practice_5.js
@@ -2,13 +2,48 @@
 
 // matrix_region_sum
 
-// Given a matrix of integers and coordinates of a rectangular region
-// within the matrix, find the sum of numbers falling inside the rectangle.
+// Given a matrix of integers and (coordinates of a rectangular region
+// within the matrix), find the sum of numbers falling inside the rectangle.
 
-function matrixRegionSum() {
+// function matrixRegionSum(matrix, topLeft, topRight) {
+//
+//
+// }
+//
+//
+// console.log(matrixRegionSum());
 
+
+// mergeSort
+
+function mergeHelper(left, right) {
+  let sorted = [];
+
+  while (left.length > 0 && right.length > 0) {
+
+    if (left[0] < right[0]) {
+      sorted.push(left.shift());
+    } else {
+      sorted.push(right.shift());
+    }
+  }
+
+  return sorted.concat(left).concat(right);
 
 }
 
+function mergeSort(arr) {
+  if (arr.length < 2) return arr;
 
-console.log(matrixRegionSum());
+  let pivot = Math.floor(arr.length / 2);
+  let left = arr.slice(0, pivot);
+  let right = arr.slice(pivot);
+
+  let mergedLeft = mergeSort(left);
+  let mergedRight = mergeSort(right);
+
+  return mergeHelper(mergedLeft, mergedRight);
+}
+
+
+console.log(mergeSort([1, 9, 3, 2, 4, 5, 8, 7, 6, 10]));
