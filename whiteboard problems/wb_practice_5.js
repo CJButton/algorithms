8c7546364b6a11fe1e7a4fc6e2a2ead3cbd45541
CJButton <gsyme1907@gmail.com>
2017-04-11 00:49:44 -0700

    productify completed in o of n

--- whiteboard problems/wb_practice_5.js
+++ whiteboard problems/wb_practice_5.js
@@ -1,4 +1,61 @@
 
+// Productify
+// given an array of numbers, replace all the numbers with the product
+// of all the other numbers
+
+// const productify = (arr) => {
+//
+//   let results = [];
+//
+//   for (let i = 0; i < arr.length; i++) {
+//     product = null;
+//
+//     if (i === 0) {
+//       product = arr.slice(i + 1).reduce(function(acc, val) {
+//         return acc * val;
+//       });
+//     } else if ( i === arr.length - 1) {
+//       product = arr.slice(0, arr.length - 1).reduce(function(acc, val) {
+//         return acc * val;
+//       });
+//     } else {
+//       product = arr.slice(0, i).concat(arr.slice(i + 1, arr.length)).reduce(
+//         function(acc, val) {
+//           return acc * val;
+//         }
+//       );
+//     }
+//     results.push(product);
+//   }
+//   return results;
+//
+// }
+
+// alt version
+const productify = (arr) => {
+
+  let results = [];
+  for (let i = 0; i < arr.length; i++) {
+    results.push(1);
+  }
+
+  let top = 1;
+  for (let j = 0; j < arr.length; j++) {
+    results[j] = results[j] * top;
+    top = top * (arr[j]);
+  }
+
+  let bottom = 1;
+  for (let k = arr.length - 1; k >= 0; k--) {
+    results[k] = bottom * results[k];
+    bottom = bottom * arr[k];
+  }
+
+  return results;
+}
+console.log(productify([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
+//-----------------------
+
 
 // matrix_region_sum
 
@@ -13,6 +70,7 @@
 //
 // console.log(matrixRegionSum());
 
+//------------------------
 
 // mergeSort
 
@@ -46,4 +104,27 @@ function mergeSort(arr) {
 }
 
 
-console.log(mergeSort([1, 9, 3, 2, 4, 5, 8, 7, 6, 10]));
+// console.log(mergeSort([1, 9, 3, 2, 4, 5, 8, 7, 6, 10]));
+
+// ------------
+// Binary Search
+// array must already be sorted before search can take place
+
+const binarySearch = (arr, target) => {
+  let mid = Math.floor(arr.length / 2);
+
+  if (target < arr[mid]) {
+    mid = binarySearch(arr.slice(0, mid), target);
+  } else if (target === arr[mid]) {
+    return mid;
+  } else if (target > arr[mid]){
+    mid += binarySearch(arr.slice(mid), target);
+  }
+
+  return mid;
+
+}
+
+// console.log(binarySearch([1, 2, 3, 4, 5], 1)); // 0
+// console.log(binarySearch([1, 2, 3, 4, 5], 3)); // 2
+// console.log(binarySearch([1, 2, 3, 4, 5], 5)); // 4
---
+++ whiteboard problems/wb_practice_6.js
@@ -0,0 +1,14 @@
+
+
+// find all subsets of an array
+// how many will it return
+
+const subsets = (arr) => {
+
+
+}
+
+console.log(subsets([1, 2, 3, 4]));
+[2]
+[2, 3]
+[2, 3, 4]
