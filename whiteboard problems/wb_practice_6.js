7fd28baa22453bcdcd7b5b9e2cf250010df8770f
CJButton <gsyme1907@gmail.com>
2017-04-11 23:20:50 -0700

    longest palindrome

--- whiteboard problems/wb_practice_6.js
+++ whiteboard problems/wb_practice_6.js
@@ -1,5 +1,31 @@
 
 
+// Write a function that will take a string and return the indices of
+// the start/end of the longest palindrome it contains.
+// Time complexity: O(n^2)
+
+
+const longestPalindrome = (string) => {
+
+  let start = 0;
+  let palLength = 2;
+
+  for (let i = 0; i < string.length - 2; i++) {
+    for (let j = 2; (i + j) < string.length - 1; j++) {
+      if (string.slice(i, j) === string.slice(i, j).split("").reverse().join("")
+          && j - i > palLength){
+            start = i;
+            palLength = j - 1
+      }
+    }
+  }
+  return [start, palLength];
+
+}
+
+console.log(longestPalindrome('acapella')); // should return [0, 2]
+
+
 // find all subsets of an array
 // how many will it return?
 
@@ -16,4 +42,4 @@ const subsets = (array) => {
    return subSubsets.concat(newSubsets);
 
 }
-console.log(subsets([1, 2, 3, 4]));
+// console.log(subsets([1, 2, 3, 4]));
