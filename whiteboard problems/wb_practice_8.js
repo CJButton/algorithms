741c1027f2dd19056ce880e9515aae1f8e699851
CJButton <gsyme1907@gmail.com>
2017-04-16 18:29:34 -0700

    working on bubledown for binaryheap

--- whiteboard problems/wb_practice_8.js
+++ whiteboard problems/wb_practice_8.js
@@ -8,23 +8,45 @@ class BinaryMaxHeap {
 
   push(el) {
     this.content.push(el);
-    this.bubbleup(el);
+    this.bubbleup(this.content.length - 1, el);
   }
 
   // inserting a new object into the heap
-  bubbleup(el) {
-    let prnt = this.parent();
-    console.log(`${el}'s parent is ${prnt}`);
+  bubbleup(childIdx, el) {
+    let prnt = this.parent(childIdx);
+
+    if (el > this.content[prnt]) {
+      let swap = this.content[prnt];
+      this.content[childIdx] = swap
+      this.content[prnt] = el;
+      this.bubbleup(prnt, el)
+    }
+  }
+
+  delete() {
+    this.content[0] = this.content[this.content.length - 1];
+    this.bubbledown(0, this.content[0]);
+    this.content.pop();
+  }
+
+  bubbledown(idx, el) {
+    let children = this.children(idx);
+  }
+
+  children(idx) {
+    let left = (idx * 2) + 1;
+    let right = (idx * 2) + 2;
+    return [left, right];
 
   }
 
-  parent() {
-    let parentN = Math.floor((this.content.length + 1) / 2) - 1;
+  parent(childIdx) {
+    let parentN = Math.floor((childIdx + 1) / 2) - 1;
     let parent = this.content[parentN];
     if (this.content.length === 1) {
       return null;
     } else {
-      return parent;
+      return parentN;
     }
 
   }
@@ -36,6 +58,9 @@ heap.push(5)
 heap.push(6)
 heap.push(4)
 heap.push(3)
+heap.push(7)
+heap.push(8)
+heap.delete();
 console.log(heap);
 
 // I give you a scrambled list of n unique integers between 0 and n.
@@ -53,7 +78,7 @@ const missing2 = (arr) => {
   return real - total - 1;
 }
 
-console.log(missing2([1, 5, 9, 7, 6, 2, 3, 8])); // => 4 is missing
+// console.log(missing2([1, 5, 9, 7, 6, 2, 3, 8])); // => 4 is missing
 
 
 // O(n) for time
@@ -74,4 +99,4 @@ const missing = (arr) => {
   return num;
 }
 
-console.log(missing([1, 5, 9, 7, 6, 2, 3, 8])); // => 4 is missing
+// console.log(missing([1, 5, 9, 7, 6, 2, 3, 8])); // => 4 is missing
