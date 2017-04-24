360be5e41638c08c65477d23879f1793b6bffb00
CJButton <gsyme1907@gmail.com>
2017-04-23 19:46:34 -0700

    resize unshift and shift work

--- whiteboard problems/dynamic.js
+++ whiteboard problems/dynamic.js
@@ -15,51 +15,65 @@ class Dynamic {
     this.index = 0;
   }
 
+  // works
   push(el) {
     this.arr[this.index] = el;
     if (this.index + 1 >= this.size) {
-      resize();
+      this.resize();
     }
     this.index += 1;
   }
 
+  // works
   pop() {
-    this.arr[this.index] = null;
+    this.arr[this.index - 1] = null;
     this.index -= 1;
   }
 
+  // works
   shift() {
-    for (var i = 1; i < this.arr.length; i++) {
-      this.arr[i - 1] = this.arr[i];
+    for (var i = 0; i < this.index; i++) {
+      this.arr[i] = this.arr[i + 1];
     }
     this.index -= 1;
   }
 
+  // works
   unshift(el) {
-    if (this.arr.length + 1 === this.size) {
-      resize(1); // make enough space for the unshifted element
-    } else {
-      for (var i = this.arr.length - 1; i > 1; i--) {
-        this.arr[i] = this.arr[i - 1];
-      }
+    if (this.index + 1 >= this.size) {
+      this.resize(); // make enough space for the unshifted element
+    }
+
+    for (var i = this.index; i >= 1; i--) {
+      this.arr[i] = this.arr[i - 1];
     }
     this.arr[0] = el;
+    this.index += 1;
   }
 
-  // n here is the starting point for when we move all els to new array
-  resize(n = 0) {
+  // works
+  resize() {
     let newArr = [];
-    for (var i = 1; i < this.size * 2; i++) {
+    for (var i = 0; i < this.size * 2; i++) {
       newArr.push(null);
     }
-    for (var i = n; i < newArr.length; i++) {
+
+    for (var i = 0; i <= this.index; i++) {
       newArr[i] = this.arr[i];
     }
+    this.size = this.size * 2;
     this.arr = newArr;
   }
-
-
 }
 
 let dynamic = new Dynamic()
+dynamic.push(5)
 console.log(dynamic);
+dynamic.unshift(25)
+dynamic.unshift(10)
+dynamic.unshift(15)
+dynamic.unshift(20)
+console.log(dynamic);
+// console.log(dynamic);
+// console.log(dynamic);
+// dynamic.unshift(15)
