e74d28c07848a03df95d296d1a4333f6b1c2f951
CJButton <gsyme1907@gmail.com>
2017-05-20 23:08:38 -0500

    is unique problem

--- whiteboard problems/hr.js
+++ whiteboard problems/hr.js
@@ -1,4 +1,14 @@
 
+// p. 90
+// 1.1 Is Unique?
+// Implement an algorithm to determine if a string has all unique characters.
+// What if you cnnot use additional data structures?
+
+// Answer 1: iterate through each indidiviudal element in the string,
+// and add them to a set. Check if said character is already in the Set.
+// Answer 2: 
+
+
 
 
 // Arrays: Left Rotation
